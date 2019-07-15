75dfec0ea37356c68fafee8712d614527d0d33dd
j <jlawrence6809@yahoo.com>
2019-07-15 22:48:52 +0100

    modified reading logs. added interval. updated package.json

--- DataWebSite/routes/data.js
+++ DataWebSite/routes/data.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var express = require('express');
 var router = express.Router();
 
-const LOG_ROOT = "/Users/jeremy/code/gardening/DataWebSite/testlogs/";
+const LOG_ROOT = "/home/pi/logs/";
 
 
 let GLOBAL_DATA = [];
@@ -15,14 +15,15 @@ let refreshData = () => {
 		.map(s => JSON.parse(s))
 		.forEach(arr => arr.forEach(r => dataObj.push(r)));
 
-	dataObj.forEach( d => {
-      try {
-        d.fields = eval("x = " + d.body);
-      } catch(e) {
-      	console.error('bad row!', d);
-        return;
-      }
-    });
+// for local dev
+// dataObj.forEach( d => {
+ //      try {
+ //        d.fields = eval("x = " + d.body);
+ //      } catch(e) {
+ //      	console.error('bad row!', d);
+ //        return;
+ //      }
+ //    });
 
     let intIdToHex = (id) => "0x" + id.toString(16);
 
@@ -31,10 +32,10 @@ let refreshData = () => {
     let devices = {};
     let idToIdx = {};
     dataObj.forEach( d =>
-      Object.keys(d.fields)
+      Object.keys(d.body)
         .forEach(k => {
           if(k === 'id'){
-            devices[intIdToHex(d.fields.id)] = true;
+            devices[intIdToHex(d.body.id)] = true;
           } else {
             tablesData[k] = [];
           }
@@ -48,23 +49,21 @@ let refreshData = () => {
 
     // fill in tables data with data
     dataObj.forEach( d => {
-      let idx = headers.indexOf(intIdToHex(d.fields.id));
+      let idx = headers.indexOf(intIdToHex(d.body.id));
       Object.keys(tablesData).forEach( td => {
         let tableData = tablesData[td];
         let row = new Array(headers.length).fill(null);
         row[0] = new Date(d.time);
-        row[idx] = d.fields[td];
+        row[idx] = d.body[td];
         tableData.push(row);
       });
     });
 
     GLOBAL_DATA = tablesData;
 };
+
 refreshData();
-	/*
-	"time": 1562008193853,
-		"body": "{id: 0xe0286c12cfa4, a0: 1.85, a3: 1.96, a6: -1.00, a7: -1.00, a4: 0.48, a5: 0.27, t0: 86.98, t3: 93.04, t6: nan, t7: nan, t4: 13.78, t5: -5.30, th: 109, h: 27 }null"
-	*/
+setInterval(refreshData, 1000*60*5);
 router.get('/', function(req, res, next) {
 	res.send(GLOBAL_DATA);
 	// res.send([
