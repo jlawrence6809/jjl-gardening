3a25d0e3e442e1c06bc090fe0acd563f15f88cec
j <jlawrence6809@yahoo.com>
2025-08-12 22:38:24 -0700

    Add build, upload, monitor scripts

---
+++ Sunroom2/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Usage: ./monitor.sh [env]
+# Example: ./monitor.sh nodemcu-32s
+
+if [[ "$1" == "nodemcu-32s" || "$1" == "esp32-s3-devkit" ]]; then
+  ENV="$1"
+elif [[ -z "$1" ]]; then
+  ENV="esp32-s3-devkit"
+else
+  echo "Error: ENV must be 'nodemcu-32s' or 'esp32-s3-devkit'"
+  exit 1
+fi
+
+pio run -e "$ENV"
